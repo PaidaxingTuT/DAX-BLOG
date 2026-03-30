a24cfc3d8350c61c54c2a65fb47b7c6bb6d10e60
AmaneKanata <paidaxing_tut@outlook.com>
2026-03-30 14:00:31 +0800

    feat: 规范回答格式

--- src/main/java/com/daxtech/daxblog/controller/ChatController.java
+++ src/main/java/com/daxtech/daxblog/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.daxtech.daxblog.controller;
 
+import com.daxtech.daxblog.enums.ResultCodeEnum;
+import com.daxtech.daxblog.response.Response;
 import com.daxtech.daxblog.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +17,12 @@ public class ChatController {
     private final ChatService chatService;
 
     @GetMapping("/ask")
-    public String chat(@RequestParam String message) {
-        return chatService.getMessage(message);
+    public Response<String> chat(@RequestParam String message) {
+        String msg = chatService.getMessage(message);
+        if (msg.equals("无法回答。")) {
+            return Response.faildata(ResultCodeEnum.FAIL,msg);
+        }
+        return Response.success(ResultCodeEnum.SUCCESS,msg);
 
     }
 }
